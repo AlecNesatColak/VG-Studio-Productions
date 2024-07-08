546d8d514ee0fd17b85482e574983af2a6575882
Alec-Nesat Colak <acolak9@ufl.edu>
2024-07-08 10:18:19 -0400

    Added MongoDB

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
 const app = express();
+require("dotenv").config();
+const dbConfig = require("./dbConfig/dbConfig");
+
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => console.log(`Listening on port ${port}, server is running.`));
