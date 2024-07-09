5c945c63b87273a55ec5f82c2e325897da12216b
Alec-Nesat Colak <acolak9@ufl.edu>
2024-07-09 18:05:43 -0400

    Added admin and page functionality and structure

---
+++ backend/Routes/adminRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.error(error, "error registering");  
+  }
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AdminRegister from "./AdminPages/AdminRegister/Register";
+import Home from "./Pages/Home/Home";
 import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Navbar>App</Navbar>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Example home route */}
+        <Route path="/admin-register" element={<AdminRegister />} />
+        {/* Route for AdminRegister component */}
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
